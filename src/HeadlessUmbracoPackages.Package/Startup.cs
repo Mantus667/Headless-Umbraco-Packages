7e4439ec5ce1fe21f9ab5dad29fcd95f3047c97a
David Brendel <mantus667@googlemail.com>
2017-05-17 13:02:15 +0200

    Created package project
    Created package project which will hold our demo package code like
    installer, migrations and more

---
+++ src/HeadlessUmbracoPackages.Package/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Umbraco.Core;
+
+namespace HeadlessUmbracoPackages.Package
+{
+    /// <summary>
+    /// This class is called by Umbraco when the website starts we handle installation here
+    /// </summary>
+    /// <seealso cref="Umbraco.Core.ApplicationEventHandler" />
+    public class Startup : ApplicationEventHandler
+    {
+        /// <summary>
+        /// Overridable method to execute when Bootup is completed, this allows you to perform any other bootup logic required for the application.
+        /// Resolution is frozen so now they can be used to resolve instances.
+        /// </summary>
+        /// <param name="umbracoApplication"></param>
+        /// <param name="applicationContext"></param>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            
+        }
+    }
+}
