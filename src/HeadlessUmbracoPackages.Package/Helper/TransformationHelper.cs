88af3030adee68463b133cedf1cf993f412b367e
David Brendel <mantus667@googlemail.com>
2017-05-17 15:49:47 +0200

    Installer/Uninstaller
    Added installer/uninstaller
    Added helper for config transformations
    Added transformation for section dashboard

---
+++ src/HeadlessUmbracoPackages.Package/Helper/TransformationHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web;
+using Microsoft.Web.XmlTransform;
+using Umbraco.Core.Logging;
+
+namespace HeadlessUmbracoPackages.Package.Helper
+{
+    /// <summary>
+    /// Transformation helper which helps doing xdt transformation
+    /// </summary>
+    internal class TransformationHelper
+    {
+        /// <summary>
+        /// Transforms the specified file with the given transformation.
+        /// </summary>
+        /// <param name="fileToTransform">The file to transform.</param>
+        /// <param name="transformationFile">The transformation file.</param>
+        public static void Transform(string fileToTransform, string transformationFile)
+        {
+            //LogHelper.Info<TransformationHelper>($"Starting tranformation of {fileToTransform} with {transformationFile}");
+
+            var sourceDocFileName = VirtualPathUtility.ToAbsolute(fileToTransform);
+            var xdtFileName = VirtualPathUtility.ToAbsolute(transformationFile);
+
+            try
+            {
+                // The translation at-hand
+                using (var xmlDoc = new XmlTransformableDocument())
+                {
+                    xmlDoc.PreserveWhitespace = true;
+                    xmlDoc.Load(HttpContext.Current.Server.MapPath(sourceDocFileName));
+
+                    using (var xmlTrans = new XmlTransformation(HttpContext.Current.Server.MapPath(xdtFileName)))
+                    {
+                        if (xmlTrans.Apply(xmlDoc))
+                        {
+                            // If we made it here, sourceDoc now has transDoc's changes
+                            // applied. So, we're going to save the final result off to
+                            // destDoc.
+                            xmlDoc.Save(HttpContext.Current.Server.MapPath(sourceDocFileName));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error<TransformationHelper>("There was an error during transformation", ex);
+            }
+
+            LogHelper.Info<TransformationHelper>("Transformation finished successfully");
+        }
+    }
+}
---
+++ src/HeadlessUmbracoPackages.Package/Uninstaller.cs
@@ -0,0 +1,51 @@
+﻿using HeadlessUmbracoPackages.Package.Helper;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+
+namespace HeadlessUmbracoPackages.Package
+{
+    /// <summary>
+    /// Uninstaller used to uninstall stuff from config files and other places
+    /// </summary>
+    internal class Uninstaller
+    {
+        /// <summary>
+        /// Runs this installer and checks if an install is necessary.
+        /// </summary>
+        public void Run()
+        {
+            UninstallSection();
+            UninstallSectionDashboard();
+        }
+
+        /// <summary>
+        /// Uninstalls the custom section created by the package.
+        /// </summary>
+        private void UninstallSection()
+        {
+            LogHelper.Info<Installer>("Try to uninstall section for DemoPackage");
+            var services = ApplicationContext.Current.Services;
+
+            var sectionService = services.SectionService;
+            var userService = services.UserService;
+
+            userService.DeleteSectionFromAllUsers("demoPackage");
+            var section = sectionService.GetByAlias("demoPackage");
+            sectionService.DeleteSection(section);
+
+            LogHelper.Info<Installer>("Uninstalled the section.");
+        }
+
+        /// <summary>
+        /// Installs the custom section dashboard.
+        /// </summary>
+        private void UninstallSectionDashboard()
+        {
+            LogHelper.Info<Installer>("Try to install section dashboard for DemoPackage");
+
+            TransformationHelper.Transform("~/config/dashboard.config", "~/App_Plugins/DemoPackage/Transformations/dashboard.uninstall.xdt");
+
+            LogHelper.Info<Installer>("Done installing section dashboard for DemoPackage");
+        }
+    }
+}
